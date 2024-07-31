4631a0759cfbd3da82fb1a392d1118b5469158ff
Kira Zvereva <angryvex@gmail.com>
2024-08-01 01:17:35 +0300

    Fix hold-drag drawing

--- script.js
+++ script.js
@@ -1,47 +1,99 @@
 const container = document.querySelector(".draw-container");
+const gridSizeButton = document.querySelector(".btn");
 
-// let gridSize = prompt('How many rows in a grid?', 5);
+gridSizeButton.addEventListener("click", getGridSize);
 
-// while (!(gridSize > 0 && gridSize < 101)) {
-//     gridSize = prompt('Please try again');
-// }
-
-let gridSize = 11;
-
-buildGrid(gridSize);
-
-for (let i = 0; i < gridSize * gridSize; i++) {
-    const div = document.createElement("div");
-    div.classList.add("draw");
-    container.appendChild(div);
+function getGridSize() {
+    let gridSize = prompt("How many rows in a grid?", 5);
+    if (!(gridSize > 0 && gridSize < 101)) {
+        gridSize = prompt("Please try again");
+    }
+    if (container.firstChild != undefined) {
+        removeCells();
+    }
+    buildGrid(gridSize);
+    const drawingCells = document.querySelectorAll(".draw");
+    reset(drawingCells);
+    dragPaintCells(drawingCells);
 }
-
-const drawingCells = document.querySelectorAll(".draw");
+// let gridSize = 11;
 
 function buildGrid(gridSize) {
-    container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
-}
-
-drawingCells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-        cell.classList.add("painted");
-    });
-});
-
-let MDOWN = false;
-
-["mousedown", "mouseup"].forEach((e) => {
-    drawingCells.forEach((cell) => {
-        cell.addEventListener(e, () => (MDOWN = !MDOWN));
-    });
-});
-
-function paintCells() {
-    if (MDOWN) {
-        this.classList.add("painted");
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const div = document.createElement("div");
+        div.classList.add("draw");
+        container.appendChild(div);
+        container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
     }
 }
 
-drawingCells.forEach((cell) => {
-    cell.addEventListener("mouseenter", paintCells);
-});
+// buildGrid(gridSize);
+// const drawingCells = document.querySelectorAll('.draw');
+
+// function getDrawingCells() {
+// return drawingCells;
+
+// }
+
+// function fillCells(drawingCells) {
+//     drawingCells.forEach((cell) => {
+//         cell.addEventListener('click', () => {
+//             cell.classList.add('painted');
+//         });
+//     })
+// }
+
+function reset(drawingCells) {
+    drawingCells.forEach((cell) => {
+        cell.classList.remove("painted");
+    });
+}
+
+function removeCells() {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+
+let isPainting = false;
+
+function dragPaintCells(drawingCells) {
+    drawingCells.forEach((cell) => {
+        cell.addEventListener("mousedown", (e) => {
+            isPainting = true;
+        });
+        cell.addEventListener("mouseup", (e) => {
+            cell.classList.add("painted");
+            isPainting = false;
+        });
+        cell.addEventListener("mousemove", () => {
+            if (!isPainting) {
+                return;
+            } else {
+                cell.classList.add("painted");
+            }
+        });
+    });
+}
+
+// let MDOWN = false;
+
+// ['mousedown', 'mouseup'].forEach(e => {
+//     drawingCells.forEach((cell) => {
+//         cell.addEventListener(e, () => MDOWN = !MDOWN)
+//     });
+// });
+
+// function paintCells() {
+//     if (MDOWN) {
+//         this.classList.add('painted');
+//     }
+// }
+
+// drawingCells.forEach((cell) => {
+//     cell.addEventListener('click', paintCells);
+//     console.log('clicked')
+// })
